11cc2d023e919b460c9867d63f1620e27b1765e4
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2021-10-11 15:06:19 -0700

    Made decryption require typing out the password

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ function encryptText() {
     let password = document.querySelector("#passInput").value
     let passLocation = document.querySelector('input[name="passLocation"]:checked').value
     let encryptedText = ""
-    let passCharCode = 0;
-    for (let i = 0; i < password.length; i++) {
-      passCharCode += password.charCodeAt(i)
-    }
+    let passCharCode = addCharCode(password)
+
     for (let i = 0; i < originalText.length; i++) {
-        encryptedText += String.fromCharCode(originalText.charCodeAt(i) - passCharCode - 1)
+        encryptedText += String.fromCharCode(originalText.charCodeAt(i) - passCharCode)
     }
     if (passLocation === "beginning") {
       encryptedText = password + encryptedText
@@ -20,24 +18,31 @@ function encryptText() {
 
 function decryptText() {
   let originalText = document.querySelector("#encryptedInput").value
-  let passLength = document.querySelector("#passLength").value
+  let password = document.querySelector("#decryptPass").value
   let passLocation = document.querySelector('input[name="encryptedPassLocation"]:checked').value
   let decryptedText = ""
-  let passCharCode = 0;
+  let passCharCode = addCharCode(password);
+
+  if (password.length > originalText.length - 1) {
+    document.querySelector("h3").textContent = "Invalid Password"
+    return
+  }
 
   if (passLocation === "beginning") {
-    for (let i = 0; i < passLength; i++) {
-      passCharCode += originalText.charCodeAt(i)
-    }
-    originalText = originalText.slice(passLength)
+    originalText = originalText.slice(password.length)
   } else {
-    for (let i = originalText.length; password.length - i < passLength; i--) {
-      passCharCode += originalText.charCodeAt(i)
-    }
-    originalText = originalText.slice(0, originalText.length - passLength)
+    originalText = originalText.slice(0, originalText.length - password.length)
   }
   for (let i = 0; i < originalText.length; i++) {
-    decryptedText += String.fromCharCode(originalText.charCodeAt(i) + passCharCode + 1)
+    decryptedText += String.fromCharCode(originalText.charCodeAt(i) + passCharCode)
   }
   document.querySelector("h3").textContent = decryptedText
+}
+
+function addCharCode(text) {
+  let charCode = 0
+  for (let i = 0; i < text.length; i++) {
+    charCode += text.charCodeAt(i)
+  }
+  return charCode
 }
